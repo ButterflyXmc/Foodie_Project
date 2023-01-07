8dd03be8f41417ce49dd08be2027ad49c9748028
mahic <mahic@domain.com>
2023-01-07 01:23:38 -0700

    Working on resto edit

--- src/components/RestEditAcct.vue
+++ src/components/RestEditAcct.vue
@@ -7,6 +7,14 @@
 
   <!-- need a div for the GET to work -->
   <div>
+    <section v-for="restaurant in restaurant" :key="restaurant.restaurantId">
+      <h2>Name : {{ restaurant.name }}</h2>
+      <p>Address : {{ restaurant.address }}</p>
+      <p>Bio : {{ restaurant.bio }}</p>
+      <p>City : {{ restaurant.city }}</p>
+      <p>Email : {{ restaurant.email }}</p>
+      <p>Phone number : {{ restaurant.phoneNum }}</p>
+    </section>
     <h1>Edit Account</h1>
     <v-row>
       <v-col cols="12" md="10">
@@ -26,13 +34,13 @@
         <v-text-field v-model="city" label="City"></v-text-field>
       </v-col>
 
-      <v-col cols="12" md="10">
+      <!-- <v-col cols="12" md="10">
         <v-text-field v-model="email" label="Email"></v-text-field>
-      </v-col>
+      </v-col> -->
 
-      <v-col cols="12" md="10">
+      <!-- <v-col cols="12" md="10">
         <v-text-field v-model="password" label="Password"></v-text-field>
-      </v-col>
+      </v-col> -->
 
       <v-col cols="12" md="10">
         <v-text-field v-model="phoneNum" label="Phone Number : XXX-XXX-XXX">
@@ -50,18 +58,17 @@
 <script>
 import axios from "axios";
 import cookies from "vue-cookies";
-import router from "@/router";
+// import router from "@/router";
 
 export default {
   name: "RestEditAcct",
   data() {
     return {
+      restaurant: "",
       name: "",
       address: "",
       bio: "",
       city: "",
-      email: "",
-      password: "",
       phoneNum: "",
     };
   },
@@ -73,7 +80,8 @@ export default {
           method: "PATCH",
           headers: {
             "x-api-key": process.env.VUE_APP_API_KEY,
-            token: cookies.get(`token`),
+            // Maybe I need to put the cookies in a function for it to load when the page load??
+            // token: cookies.get(`token`),
           },
           data: {
             token: this.token,
@@ -81,20 +89,45 @@ export default {
             address: this.address,
             bio: this.bio,
             city: this.city,
-            email: this.email,
-            password: this.password,
+            // email: this.email,
+            // password: this.password,
             phoneNum: this.phoneNum,
           },
         })
         .then((response) => {
           console.log(response);
-          router.push("/discover-page");
+          alert("Saved!");
         })
         .catch((error) => {
           console.log(error);
           alert("error!");
         });
     },
+    getAcct() {
+      axios
+        .request({
+          url: "https://foodierest.ml/api/restaurant",
+          method: "GET",
+          headers: {
+            "x-api-key": process.env.VUE_APP_API_KEY,
+            token: cookies.get(`token`),
+          },
+          // !!!! not sure
+          // params: {
+          //   restaurantId: this.restaurantId,
+          // },
+        })
+        .then((response) => {
+          this.restaurant = response.data;
+        })
+        .catch(() => {
+          alert("Error!");
+        });
+    },
+  },
+
+  mounted() {
+    this.getAcct();
   },
 };
 </script>
